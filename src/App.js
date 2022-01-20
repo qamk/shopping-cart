22045a044537e7f4eb612a5ee182372571112dbd
qamk <qamking@gmail.com>
2022-01-20 14:36:10 +0000

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import MainLayout from './components/MainLayout'
+import Main from './components/Main';
+import MainLayout from './components/MainLayout';
+import Home from './components/Home'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <MainLayout />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="home" element={ <Home /> }></Route>
+          <Route path="shop" element={<Main />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
