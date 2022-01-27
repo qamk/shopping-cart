beeb3f2dfbca8326a9539cc8d82147c83531b9b3
qamk <qamking@gmail.com>
2022-01-27 09:57:46 +0000

    Refactor and extract helper methods

--- src/assets/helpers/helperMethods.js
+++ src/assets/helpers/helperMethods.js
@@ -12,4 +12,92 @@ const extractItemIndex = (cartOfObjects, key) => {
   return itemInfo;
 }
 
-export { clone, extractItemIndex };
+const createTooltip = ({styles = {}, fade = false}) => {
+
+  let myTooltip = document.createElement('div');
+
+  myTooltip.classList.add('tooltip');
+
+  Object.entries(styles).forEach(([property, value]) => {
+    myTooltip.style[property] = value;
+  })
+
+  if (fade) {
+    myTooltip.style.opacity = 1;
+  }
+
+  return myTooltip
+
+}
+
+
+const typeOf = (obj) => {
+  return toString.call(obj).slice(8, -1).toLowerCase();
+}
+
+const isFalsy = (value) => {
+  const objType = typeOf(value);
+  let falsy;
+  switch (objType) {
+    case "array":
+      falsy = (value.length === 0)
+      break;
+    case "object":
+      falsy = (value.keys().length === 0)
+      break
+    default:
+      falsy = ( (value === undefined) || (value === null))
+      break;
+  }
+  return falsy;
+}
+
+const cartValuesIdentical = (cart, storage) => {
+  if (isFalsy(storage) || isFalsy(cart)) {
+    return false;
+  };
+
+  if (cart.length !== storage.length) {
+    return false;
+  }
+
+  const filtered = storage.filter((obj, index) => {
+    const comparableObj = cart[index];
+    let idVerdict = (obj.id === comparableObj.id)
+    let quantVerdict = (obj.quantity === comparableObj.quantity)
+    return (idVerdict && quantVerdict)
+  })
+  return (cart.length === filtered.length);
+}
+
+const sumController = ({list = null, type='quantity'}) => {
+  const cart = list || JSON.parse(sessionStorage.getItem('cart'));
+  const methods = {
+    'quantity': countStorage,
+    'cost': calculateCost 
+  }
+  
+  if(!isFalsy(cart)) {
+    return methods[type](cart);
+  }
+
+  return 0;
+} 
+
+const countStorage = (cart) => {
+  let count = 0;
+  cart.forEach((item) => {
+      count += item.quantity;
+  })
+  return count;
+}
+
+const calculateCost = (cart) => {
+  let total = 0;
+  cart.forEach((item) => {
+    total += (item.quantity * item.cost);
+  })
+  return total;
+}
+
+export { clone, extractItemIndex, isFalsy, sumController, createTooltip, cartValuesIdentical };
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,45 +1,6 @@
 import { useEffect, useState } from 'react';
 import ProductList from './ProductList';
-import { clone, extractItemIndex } from '../assets/helpers/helperMethods';
-
-const typeOf = (obj) => {
-  return toString.call(obj).slice(8, -1).toLowerCase();
-}
-
-const isFalsy = (value) => {
-  const objType = typeOf(value);
-  let falsy;
-  switch (objType) {
-    case "array":
-      falsy = (value.length === 0)
-      break;
-    case "object":
-      falsy = (value.keys().length === 0)
-      break
-    default:
-      falsy = ( (value === undefined) || (value === null))
-      break;
-  }
-  return falsy;
-}
-
-const cartValuesIdentical = (cart, storage) => {
-  if (isFalsy(storage) || isFalsy(cart)) {
-    return false;
-  };
-
-  if (cart.length !== storage.length) {
-    return false;
-  }
-
-  const filtered = storage.filter((obj, index) => {
-    const comparableObj = cart[index];
-    let idVerdict = (obj.id === comparableObj.id)
-    let quantVerdict = (obj.quantity === comparableObj.quantity)
-    return (idVerdict && quantVerdict)
-  })
-  return (cart.length === filtered.length);
-}
+import { clone, extractItemIndex, isFalsy, cartValuesIdentical } from '../assets/helpers/helperMethods';
 
 const Main = () => {
   const [cartItems, setCartItems] = useState([]);
@@ -68,8 +29,6 @@ const Main = () => {
       newItems = [...cartItems, item];
     }
     sessionStorage.setItem('cart', JSON.stringify(newItems))
-    console.log('Adding to cart item:');
-    console.dir(newItems);
     setCartItems(newItems);
   }
 
