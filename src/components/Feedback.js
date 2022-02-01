ff8716df0b94280ebfa15104d13c8fbdd8e02a11
qamk <qamking@gmail.com>
2022-02-01 13:13:53 +0000

    Remove delay logic and dependency

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,27 +1,10 @@
-import { useEffect, useState } from "react";
-
-const Feedback = ({message = "A message", delay = null}) => {
-  const [display, setDisplay] = useState(true);
-  
-  useEffect(() => {
-    if (delay && display) {
-      setTimeout(() => {
-        setDisplay(false);
-      }, 4 * 1000)
-    }
-  })
-  
+const Feedback = ({message = "A message"}) => {  
 
   return(
     <>
-      { display
-        ? (
-            <p className="content is-small">
-              {message}
-            </p>
-          )
-        : null
-      }
+      <p className="content is-small">
+        {message}
+      </p>
     </>
   )
 
